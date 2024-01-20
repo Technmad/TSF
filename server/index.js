29b11ce41e919dbac93d6e241e06afd06927fccd
anurag <anuragpathakoffl@gmail.com>
2024-01-21 03:14:00 +0530

    added server page

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// initialize app
+const app = express();
+
+//env variables
+dotenv.config();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//app listening
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening to port ${port}...`));
